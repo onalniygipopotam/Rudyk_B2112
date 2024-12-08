fc99ede767dbf98f3642c59eb476eee828d7f4fa
ADMIN <nikitarudik61@gmail.com>
2024-12-08 15:37:02 +0200

    first commit

---
+++ dz1.py
@@ -0,0 +1,19 @@
+#1006
+# x, y = input().split()
+# x, y = int(x), int(y)
+# print(x - y)
+
+#1005
+# x, y = input().split()
+# x, y = int(x), int(y)
+# result = x**3 + (x + 1) / (y**2 + 1)
+# print(f"{result:.1f}")
+
+#1004
+# p = float(input())
+# rounded_value = round(p)
+# integer_part = int(p)
+# fractional_part = round(p - integer_part, 1)
+# print(rounded_value)
+# print(integer_part)
+# print(f"{fractional_part:.1f}")
---
+++ lesson1.py
@@ -0,0 +1,32 @@
+#1001
+#Дано чотири дійсні числа a b c d Знайти їх суму.
+# p = float(input())
+# rounded_value = round(p)
+# integer_part = int(p)
+# fractional_part = round(p - integer_part, 1)
+
+# print(rounded_value)
+# print(integer_part)
+# print(f"{fractional_part:.1f}")
+# a, b, c, d = map(float, input().split())
+# result = a + b + c + d
+# print(f"{result:.4f}")
+
+
+# a,b = map(int, input().split())
+# P = (a+b) * 2
+# print(P)
+
+
+# x, y = input().split()
+# x, y = int(x), int(y)
+# print(x % y)
+# print(y % x)
+
+
+#1007
+
+N = float(input())
+integer_part = int(N)
+result = 2 * integer_part
+print(result)
