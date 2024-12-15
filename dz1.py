3ded774daa652e1b31b2662dfccdab097bfca643
ADMIN <nikitarudik61@gmail.com>
2024-12-15 15:16:47 +0200

    dz34

--- dz1.py
+++ dz1.py
@@ -1,3 +1,7 @@
+#dz 1 2
+
+
+
 #1006
 # x, y = input().split()
 # x, y = int(x), int(y)
@@ -17,3 +21,52 @@
 # print(rounded_value)
 # print(integer_part)
 # print(f"{fractional_part:.1f}")
+
+
+
+
+
+
+#dz 3 4
+
+
+
+
+#1011
+
+# N = int(input())
+# hundreds = N // 100
+# tens = (N // 10) % 10
+# units = N % 10
+# print(units, tens, hundreds)
+
+#1012
+
+# a = int(input())
+# if a <= 100:
+#    V = a ** 3
+#    print( V)
+# else:
+#    print( )
+
+
+#1013
+
+# x, y = map(int, input().split())
+# if abs(x) <= 1000 and abs(y) <= 1000:
+#     average = (x + y) / 2
+#     print(f"{average:.2f}")
+# else:
+#     print()
+
+
+#1014
+
+# n = int(input())
+# if 100 <= n <= 999:
+#     left_digit = n // 100
+#     remaining = n % 100
+#     new_number = remaining * 10 + left_digit
+#     print(f"{new_number:03}")
+# else:
+#    print()
