86ab3961dd1a13e9381551bdbc7b0578214cfbdb
ADMIN <nikitarudik61@gmail.com>
2025-01-12 16:24:41 +0200

    lesson9  parser + txt file

--- parser.py
+++ parser.py
@@ -1,15 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
-from pyexpat import features
 
 url = "https://uaserials.pro/films/"
-for i in range(1, 10):
-    url_new = url
-    url_new+= str(i)
-    url_new+= "/"
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, features="html.parser")
-    soup_list_name = soup.find_all('div', {'class':'th-title truncate'})
+# for i in range(1,10):
+#     url_new = url
+#     url_new+= str(i)
+#     url_new+= "/"
+r = requests.get(url)
+soup = BeautifulSoup(r.text, features="html.parser")
+soup_list_name = soup.find_all('div',{'class':'th-title truncate'})
 
-    for i in soup_list_name:
-        print(i. text)
+name_list = []
+
+for i in soup_list_name:
+    print(i.text)
+    name_list.append(i.text)
+
+f = open("film.txt", 'w')
+for film in name_list:
+    f.write(f"{film}\n")
+f.close
